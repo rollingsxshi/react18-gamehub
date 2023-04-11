9b2df9a8f3c39d8254e51a3912759dfe7cf1ed25
therollingambit <therollingambit@gmail.com>
2023-04-12 01:31:59 +0800

    display platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ const GameGrid = () => {
 
   return (
     <>
-      {error && <Text>{error}</Text>}
+      {error && <Text>{ error }</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='10px' spacing={10} >
         {games.map((g) => (
           <GameCard key={g.id} game={g} />
