1b44908970969451622e6983f15f4e142c02324c
therollingambit <therollingambit@gmail.com>
2023-04-12 01:38:58 +0800

    display critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,20 +1,26 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react"
-import { Game } from "../hooks/useGames"
-import PlatformIconList from "./PlatformIconList"
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+import CriticScore from "./CriticScore";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
-  game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden' >
+    <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{ game.name }</Heading>
-        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justifyContent={'space-between'}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
-  )
-}
-export default GameCard
+  );
+};
+export default GameCard;
